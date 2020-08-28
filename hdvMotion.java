8fb23e3dd3548467a30ba692d498fd4ee5f032d0
rajivrtk11 <rajivrtk11@gmail.com>
2020-08-28 23:58:31 +0530

    commit

---
+++ hdvMotion.java
@@ -0,0 +1,21 @@
+
+class hdvMotion{
+
+    public static void main(String args[]){
+       int x =  function(new int[4][4],0,0,3,3,"");
+       System.out.println(x);
+    }
+   static int function(int arr[][],int sx, int sy, int dx, int dy,String path){
+        if(sx > dx || sy > dy )
+           return 0;
+        if(sx == dx && sy == dy){
+            System.out.println(path);
+            return 1;
+        }
+        int count = 0;
+        count += function(arr,sx+1,sy,dx,dy,path+'H');
+        count += function(arr,sx+1,sy+1,dx,dy,path+'D');
+        count += function(arr,sx,sy+1,dx,dy,path+'V');
+        return count;
+    }
+}
