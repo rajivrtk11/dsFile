f41ac52a1257f63e4774ce079a1aa27c3196124d
rajivrtk11 <rajivrtk11@gmail.com>
2020-10-31 16:42:54 +0530

    update

---
+++ alphabetPattern/programmes/nNoPrint.java
@@ -0,0 +1,8 @@
+class nNoPrint{
+    public static void main(String[] args){
+        int num = 10;
+        for(int i = 0; i < num; i++){
+            System.out.println("num is" + i);
+        }
+    }
+}
